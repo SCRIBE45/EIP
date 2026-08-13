008f459b726ac3b499fd86eb881d57c3c60cd143
SCRIBE45 <Scribeatyale@gmail.com>
2026-08-13 19:46:33 +0200

    Update eip_trainer_mobile.py

--- eip_trainer_mobile.py
+++ eip_trainer_mobile.py
@@ -299,10 +299,6 @@ st.markdown("""
         padding-bottom: 3rem;
     }
 
-    [data-testid="stHeader"] {
-    background: transparent;
-}
-
     .titulo {
         font-size: 2.2rem;
         font-weight: 700;
